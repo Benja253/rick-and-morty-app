152f3c838a96231adc8ee5e8f6d61207615e2b59
Benja253 <benja48.9@gmail.com>
2022-03-14 18:15:58 -0300

    Project v1

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import RickAndMortyApp from './RickAndMortyApp'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RickAndMortyApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
